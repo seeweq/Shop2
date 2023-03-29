de815f55a49f1649ab60df7a52268909e865c581
seeweq <sivuyisiwet34@gmail.com>
2023-03-29 13:34:34 +0200

    created register endpoint save a user in a database,check if user exists before saved in the database;

--- src/main/java/com/project/api/Controllers.java
+++ src/main/java/com/project/api/Controllers.java
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:4200")
 public class Controllers {
     @Autowired
     private UserRepository userRepository;
--- src/main/java/com/project/config/SecurityConfig.java
+++ src/main/java/com/project/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
